ffb292c9f649f7577f9ab55816b66f0fa195ce19
rachelharu <rachel.haru9@gmail.com>
2022-11-07 15:47:07 -0800

    create search function

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('running test!!');
+const Runner = require('./runner');
+
+const runner = new Runner();
+
+const run = async () => {
+    const results = await runner.collectFiles(process.cwd());
+    console.log(results);
+};
+
+run();
